a2c3583f9aeb146c79cb4280cf377d89f0bbc679
southernwind <testakina@gmail.com>
2024-06-11 21:38:18 +0900

    アクアリウム削除

--- Back/Program.cs
+++ Back/Program.cs
@@ -1,8 +1,5 @@
 
 using System;
-using System.Linq;
-
-using Back.States;
 
 using DataBase;
 
@@ -11,44 +8,31 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Back {
-	public class Program {
-		public static void Main(string[] args) {
-			var host = CreateHostBuilder(args).Build();
-			CreateDbIfNotExists(host);
-			StartMonitoring(host);
-			host.Run();
-		}
+namespace Back; 
+public class Program {
+	public static void Main(string[] args) {
+		var host = CreateHostBuilder(args).Build();
+		CreateDbIfNotExists(host);
+		host.Run();
+	}
 
-		private static void CreateDbIfNotExists(IHost host) {
-			using var scope = host.Services.CreateScope();
-			var services = scope.ServiceProvider;
-			try {
-				var context = services.GetRequiredService<HomeServerDbContext>();
-				context.Database.EnsureCreated();
-			} catch (Exception ex) {
-				var logger = services.GetRequiredService<ILogger<Program>>();
-				logger.LogError(ex, "An error occurred creating the DB.");
-				throw;
-			}
+	private static void CreateDbIfNotExists(IHost host) {
+		using var scope = host.Services.CreateScope();
+		var services = scope.ServiceProvider;
+		try {
+			var context = services.GetRequiredService<HomeServerDbContext>();
+			context.Database.EnsureCreated();
+		} catch (Exception ex) {
+			var logger = services.GetRequiredService<ILogger<Program>>();
+			logger.LogError(ex, "An error occurred creating the DB.");
+			throw;
 		}
-		private static void StartMonitoring(IHost host) {
-			using var scope = host.Services.CreateScope();
-			var services = scope.ServiceProvider;
-			try {
-				var context = services.GetRequiredService<Monitor>();
-			} catch (Exception ex) {
-				var logger = services.GetRequiredService<ILogger<Program>>();
-				logger.LogError(ex, "An error occurred start Monitoring.");
-				throw;
-			}
-		}
+	}
 
-		public static IHostBuilder CreateHostBuilder(string[] args) {
-			return Host.CreateDefaultBuilder(args)
-				.ConfigureWebHostDefaults(webBuilder => {
-					webBuilder.UseStartup<Startup>();
-				});
-		}
+	public static IHostBuilder CreateHostBuilder(string[] args) {
+		return Host.CreateDefaultBuilder(args)
+			.ConfigureWebHostDefaults(webBuilder => {
+				webBuilder.UseStartup<Startup>();
+			});
 	}
 }
--- Back/Startup.cs
+++ Back/Startup.cs
@@ -2,7 +2,6 @@
 using System;
 
 using Back.Hubs;
-using Back.Models.Aquarium;
 using Back.Models.Common;
 using Back.Models.ElectricPower;
 using Back.Models.Financial;
@@ -10,8 +9,6 @@ using Back.Models.HealthCheck;
 using Back.Models.Kitchen;
 using Back.Models.Network;
 using Back.Models.Palmie;
-using Back.States;
-using Back.States.Monitors.Aquarium;
 
 using DataBase;
 
@@ -23,85 +20,80 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Back {
-	public class Startup {
-		private readonly string _crossOriginPolicyName = "anyOrigin";
-		public IConfiguration Configuration {
-			get;
+namespace Back; 
+public class Startup {
+	private readonly string _crossOriginPolicyName = "anyOrigin";
+	public IConfiguration Configuration {
+		get;
+	}
+
+
+	public Startup(IWebHostEnvironment env) {
+		var builder = new ConfigurationBuilder()
+			.SetBasePath(env.ContentRootPath)
+			.AddJsonFile("appsettings.json", false, true)
+			.AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true)
+			.AddEnvironmentVariables();
+		this.Configuration = builder.Build();
+
+		Console.WriteLine("設定値");
+		foreach (var section in this.Configuration.GetChildren()) {
+			Console.WriteLine($"{section.Path}:{section.Value}");
+		}
+	}
+
+	// This method gets called by the runtime. Use this method to add services to the container.
+	// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+	public void ConfigureServices(IServiceCollection services) {
+		// クロスドメインの許可
+		services.AddCors(options => {
+			options.AddPolicy(this._crossOriginPolicyName, builder => {
+				builder.AllowCredentials();
+				builder.WithOrigins(this.Configuration.GetSection("FrontOrigin").Value!);
+				builder.AllowAnyHeader();
+				builder.AllowAnyMethod();
+			});
+		});
+
+		services.AddMvc();
+		services.AddSignalR();
+		services.AddDbContext<HomeServerDbContext>(optionsBuilder => {
+			var connectionString = this.Configuration.GetConnectionString("Database");
+			optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+		});
+		services.AddLogging(builder => {
+			builder.AddConfiguration(this.Configuration.GetSection("Logging"))
+				.AddConsole()
+				.AddDebug();
+		});
+		services.AddHttpClient();
+
+		services.AddSingleton<Updater>();
+		services.AddTransient<FinancialModel>();
+		services.AddTransient<ElectricPowerModel>();
+		services.AddTransient<NetworkModel>();
+		services.AddTransient<KitchenModel>();
+		services.AddTransient<PalmieModel>();
+		services.AddTransient<HealthCheckModel>();
+	}
+
+	// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+	public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+		if (env.IsDevelopment()) {
+			app.UseDeveloperExceptionPage();
 		}
 
+		app.UseRouting();
 
-		public Startup(IWebHostEnvironment env) {
-			var builder = new ConfigurationBuilder()
-				.SetBasePath(env.ContentRootPath)
-				.AddJsonFile("appsettings.json", false, true)
-				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true)
-				.AddEnvironmentVariables();
-			this.Configuration = builder.Build();
+		app.UseCors(this._crossOriginPolicyName);
 
-			Console.WriteLine("設定値");
-			foreach (var section in this.Configuration.GetChildren()) {
-				Console.WriteLine($"{section.Path}:{section.Value}");
-			}
-		}
+		app.UseEndpoints(routes => {
+			routes.MapHub<DashboardHub>("api/hubs/dashboard-hub");
+		});
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-		public void ConfigureServices(IServiceCollection services) {
-			// クロスドメインの許可
-			services.AddCors(options => {
-				options.AddPolicy(this._crossOriginPolicyName, builder => {
-					builder.AllowCredentials();
-					builder.WithOrigins(this.Configuration.GetSection("FrontOrigin").Value!);
-					builder.AllowAnyHeader();
-					builder.AllowAnyMethod();
-				});
-			});
+		app.UseEndpoints(routes => {
+			routes.MapControllers();
+		});
 
-			services.AddMvc();
-			services.AddSignalR();
-			services.AddDbContext<HomeServerDbContext>(optionsBuilder => {
-				var connectionString = this.Configuration.GetConnectionString("Database");
-				optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-			});
-			services.AddLogging(builder => {
-				builder.AddConfiguration(this.Configuration.GetSection("Logging"))
-					.AddConsole()
-					.AddDebug();
-			});
-			services.AddHttpClient();
-
-			services.AddSingleton<Updater>();
-			services.AddSingleton<Store>();
-			services.AddSingleton<Monitor>();
-			services.AddTransient<AquariumMonitor>();
-			services.AddTransient<FinancialModel>();
-			services.AddTransient<ElectricPowerModel>();
-			services.AddTransient<NetworkModel>();
-			services.AddTransient<KitchenModel>();
-			services.AddTransient<AquariumModel>();
-			services.AddTransient<PalmieModel>();
-			services.AddTransient<HealthCheckModel>();
-		}
-
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-			if (env.IsDevelopment()) {
-				app.UseDeveloperExceptionPage();
-			}
-
-			app.UseRouting();
-
-			app.UseCors(this._crossOriginPolicyName);
-
-			app.UseEndpoints(routes => {
-				routes.MapHub<DashboardHub>("api/hubs/dashboard-hub");
-			});
-
-			app.UseEndpoints(routes => {
-				routes.MapControllers();
-			});
-
-		}
 	}
 }
